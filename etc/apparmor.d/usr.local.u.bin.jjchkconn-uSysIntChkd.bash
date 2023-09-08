a8dfcb12b12bd4e1bc4eb918ff1a2cdf34e33116
kpiq <kpiq@jj10.net>
2023-09-08 19:02:12 -0400

    modified:   etc/apparmor.d/usr.local.u.bin.jjchkconn-uSysIntChkd.bash

--- etc/apparmor.d/usr.local.u.bin.jjchkconn-uSysIntChkd.bash
+++ etc/apparmor.d/usr.local.u.bin.jjchkconn-uSysIntChkd.bash
@@ -1,4 +1,4 @@
-# Last Modified: Fri Sep  8 18:58:59 2023
+# Last Modified: Fri Sep  8 19:01:53 2023
 abi <abi/3.0>,
 
 include <tunables/global>
@@ -9,6 +9,7 @@ include <tunables/global>
   include <abstractions/consoles>
   include <abstractions/lightdm>
 
+  /proc/*/cmdline r,
   /proc/filesystems r,
   /proc/sys/kernel/random/boot_id r,
   /usr/bin/basename mrix,
@@ -18,5 +19,8 @@ include <tunables/global>
   /{,var/}run/** mrwk,
   owner /home/*/.config/uSysIntChkd/jjchkconn-uSysIntChkd.ini r,
   owner /home/*/.config/uSysIntChkd/jjchkconn-uSysIntChkd.log w,
+  owner /home/*/.config/uSysIntChkd/jjchkconn-uSysIntChkd.next r,
+  owner /home/*/.config/uSysIntChkd/jjchkconn-uSysIntChkd.pid w,
+  owner /home/*/.config/uSysIntChkd/jjchkconn-uSysIntChkd.steps r,
 
 }
