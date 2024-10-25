25e1336a19507f9965f258e2da58f408a83930a1
root <kpiq@jj10.net>
2024-10-25 07:44:47 -0400

    modified:   DEBIAN/postrm 	modified:   DEBIAN/prerm 	modified:   usr/local/u/bin/jjchkconn-fetch-alerts-and-send.bash 	modified:   usr/local/u/bin/jjchkconn-send-alerts2slack.bash 	modified:   usr/local/u/bin/jjchkconn-uSysIntChkd.bash

--- usr/local/u/bin/jjchkconn-send-alerts2slack.bash
+++ usr/local/u/bin/jjchkconn-send-alerts2slack.bash
@@ -1,10 +1,10 @@
+
 #!/bin/bash
+
+set -euo pipefail
 
 ### Author: Pedro Serrano, jj10 Net LLC, Bayamon, PR, USA
 ### USAGE: scriptname username "message"
-### DEPENDENCY: file called $uIniFile (see below).  Must
-###		contain lines containing the ChannelID and BotToken
-###		for your Slack app.
 
 if [ -z "${1}" ];
 then
@@ -20,56 +20,13 @@ else
    fi
 fi
 
-declare -g uUserHome=`getent passwd ${uUser} | cut -f6 -d:`
+readonly LOGGER="logger -t $(basename "$0")"
+log_info() { $LOGGER -p user.info "$*"; }
+log_error() { $LOGGER -p user.err "$*"; }
 
-echo -e "\nLogfile=" ${uUserHome}/.config/uSysIntChkd/`basename ${0:0:-5} | sed 's/\@//g'`.log
-exec &> ${uUserHome}/.config/uSysIntChkd/`basename ${0:0:-5} | sed 's/\@//g'`.log
-declare -g uGetEntStatus=${PIPESTATUS[0]}
-declare -g uCutStatus=${PIPESTATUS[1]}
-
-if [[ ${uGetEntStatus} -eq 0 ]] && [[ ${uCutStatus} -eq 0 ]];
-then
-   ### HOME Directory is properly configured for this user. ###
-   ### Now test whether it exists. ###
-   if [ ! -d ${uUserHome} ];
-   then
-      echo "$0: HOME Directory ${uUserHome} does not exist.  Abort..."
-      exit 64
-   fi
-else
-   echo -e "\n$0: getent exit code = ${uGetEntStatus}"
-   echo -e "\n$0: cut exit code = ${uCutStatus}"
-   echo "$0: HOME Directory ${uUserHome} not properly configured for this user.  Abort..."
-   exit 65
-fi
-
-if [ $# -ne 2 ]; then
-   echo "Abort.  The number of arguments must be strictiy two (2).";
-   echo "	 If passing arguments with whitespaces enclose text in quotes.";
-   exit 66;
-fi
-
-uHomeDir=`getent passwd $uUser|cut -f6 -d:`
-uIniFile="$uHomeDir/.config/jjchkconn-slack-alerts.ini"
-
-if [ ! -f "$uIniFile" ];
-then
-   echo "Abort.  "$uIniFile" does not exist"
-   exit 67
-fi
-
-### In case attachments are desired use the following format:
-###	curl -X POST -H 'Content-type: application/json' -F 'file=@somefile1.txt' -F 'file=@somefile2.txt' http://someurl
-
-echo -e "\n\n `hostname` - `date` $0 Attempting to send message to Slack"
-
-if [ ! -s "${2}" ];
-then
-   sh /usr/local/u/bin/sendmsg2Slack.sh "${uIniFile}" "${2}"
-   if [ $? -ne 0 ]; then
-      echo "$0.  Failed to send message to Slack.  Abort..."
-      exit 68
-   else
-      echo "$0.  Message sent to slack.com.  Success..."
-   fi
+# Slack API call with enhanced error handling
+response=$(curl -s -o /dev/null -w "%{http_code}" -X POST --data-urlencode "payload=$payload" "$webhook_url")
+if [[ "$response" -ne 200 ]]; then
+    log_error "Failed to send message to Slack. HTTP status code: $response"
+    exit 1
 fi
